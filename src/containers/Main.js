be3cd55e48bccc0d2974ac8e67fc8d01480e4d54
nemo <nemo.oconnell@networkedinsights.com>
2018-09-08 09:44:22 -0500

    add logged in logic

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,22 +4,23 @@ import { connect } from "react-redux";
 import Homepage from "../components/Homepage"
 import AuthForm from "../components/AuthForm"
 import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors"
 
 const Main = props => {
-  const { authUser } = props;
+  const { authUser, errors, removeError, currentUser } = props;
   return (
     <div className="container">
       <Switch>
-        <Route exact path="/" render={props => <Homepage {...props} /> } />
+        <Route exact path="/" render={props => <Homepage currentUser={currentUser} {...props} /> } />
         <Route exact path="/signin" render={props => {
           return (
-            <AuthForm onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
+            <AuthForm removeError={removeError} errors={errors} onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
           )
         }} />
 
         <Route exact path="/signup" render={props => {
           return (
-            <AuthForm onAuth={authUser} signUp buttonText="Sign me up!" heading="Join Warbler Today." {...props} />
+            <AuthForm removeError={removeError} errors={errors} onAuth={authUser} signUp buttonText="Sign me up!" heading="Join Warbler Today." {...props} />
           )
         }} />
       </Switch>
@@ -29,8 +30,9 @@ const Main = props => {
 
 function mapStateToProps(state) {
   return {
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    errors: state.errors
   };
 }
 
-export default withRouter(connect(mapStateToProps, { authUser })(Main));
+export default withRouter(connect(mapStateToProps, { authUser, removeError })(Main));
