5a9108818e8f85bc7dbd4e58d809139cacefb503
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-09-10 20:56:00 -0500

    add logout method

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -29,12 +29,12 @@ export default class AuthForm extends Component {
 
   render() {
     const {email, username, password, profileImageUrl} = this.state;
-    const {heading, buttonText, signUp, errors, history, removeError } = this.props;
+    const {heading, buttonText, signUp, errors, history, removeError} = this.props;
 
     history.listen(() => {
       removeError();
     });
-    
+
     return (
       <div>
         <div className="row justify-content-md-center text-center">
