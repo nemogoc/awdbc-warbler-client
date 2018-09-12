73eb4a0555546c11c51d62b38ab144a40911a881
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-09-12 07:42:51 -0500

    add delete message button and handler

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -3,7 +3,7 @@ import Moment from "react-moment";
 import {Link} from "react-router-dom";
 import DefaultProfileImg from "../images/default-profile-image.jpg"
 
-const MessageItem = ({date, profileImageUrl, text, username}) => (
+const MessageItem = ({date, profileImageUrl, text, username, removeMessage}) => (
   <div>
     <li className="list-group-item">
       <img src={profileImageUrl || DefaultProfileImg} alt={username} height="100" width="100"
@@ -16,6 +16,7 @@ const MessageItem = ({date, profileImageUrl, text, username}) => (
         </Moment>
       </span>
         <p>{text}</p>
+        <a className="btn btn-danger" onClick={removeMessage}>Delete</a>
       </div>
     </li>
   </div>
--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {fetchMessages} from "../store/actions/messages";
+import {fetchMessages, removeMessage} from "../store/actions/messages";
 import MessageItem from "../components/MessageItem"
 
 class MessageList extends Component {
@@ -8,9 +8,16 @@ class MessageList extends Component {
     this.props.fetchMessages();
   }
   render(){
-    const {messages} = this.props;
+    const {messages, removeMessage} = this.props;
     let messageList = messages.map(m => (
-      <MessageItem key={m._id} date={m.createAt} text={m.text} username={m.user.username} profileImageUrl={m.user.profileImageUrl}/>
+      <MessageItem
+        key={m._id}
+        date={m.createAt}
+        text={m.text}
+        username={m.user.username}
+        profileImageUrl={m.user.profileImageUrl}
+        removeMessage={removeMessage.bind(this, m.user._id, m._id)}
+      />
     ));
 
     return (
@@ -31,4 +38,4 @@ function mapStateToProps(state){
   };
 }
 
-export default connect(mapStateToProps, {fetchMessages})(MessageList);
+export default connect(mapStateToProps, {fetchMessages, removeMessage})(MessageList);
